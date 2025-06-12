7d8bebbdbcc20ba04d8454e1068cf25910b304f1
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-06-11 20:04:09 -0600

    Estructura, componentes y rutas

---
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SearchInputComponent } from "../../components/search-input/search-input.component";
+import { ListComponent } from "../../components/list/list.component";
+
+@Component({
+  selector: 'app-by-capital-page',
+  imports: [SearchInputComponent, ListComponent],
+  templateUrl: './by-capital-page.component.html',
+  // changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ByCapitalPageComponent {
+
+  onSearch(value: string){
+    console.log(value);
+  }
+
+}
